0a0bc63f871aa506792b940ab5b3071443c79d2f
Marin Gallien <139711467+Matiss01G@users.noreply.github.com>
2024-12-21 03:15:15 +0000

    Automatic commit before Assistant edits

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -92,4 +92,5 @@ set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/crypto_tests.dir/DependInfo.cmake"
   "CMakeFiles/store_tests.dir/DependInfo.cmake"
   "CMakeFiles/network_tests.dir/DependInfo.cmake"
+  "CMakeFiles/run_tests.dir/DependInfo.cmake"
   )
