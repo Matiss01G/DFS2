dc74b09096882027f843a7a98616bcded091a5e7
MarinGallien <22155461-MarinGallien@users.noreply.replit.com>
2025-01-20 00:51:24 +0000

    Checkpoint before revert - Modify prepare_and_send method to handle different MessageTypes
    Add unit tests for network and file server components and enable testing in CMake.

--- build/CMakeFiles/dfs_network.dir/DependInfo.cmake
+++ build/CMakeFiles/dfs_network.dir/DependInfo.cmake
@@ -9,8 +9,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/runner/workspace/src/file_server/file_server.cpp" "CMakeFiles/dfs_network.dir/src/file_server/file_server.cpp.o" "gcc" "CMakeFiles/dfs_network.dir/src/file_server/file_server.cpp.o.d"
+  "/home/runner/workspace/src/network/channel.cpp" "CMakeFiles/dfs_network.dir/src/network/channel.cpp.o" "gcc" "CMakeFiles/dfs_network.dir/src/network/channel.cpp.o.d"
+  "/home/runner/workspace/src/network/codec.cpp" "CMakeFiles/dfs_network.dir/src/network/codec.cpp.o" "gcc" "CMakeFiles/dfs_network.dir/src/network/codec.cpp.o.d"
   "/home/runner/workspace/src/network/peer_manager.cpp" "CMakeFiles/dfs_network.dir/src/network/peer_manager.cpp.o" "gcc" "CMakeFiles/dfs_network.dir/src/network/peer_manager.cpp.o.d"
   "/home/runner/workspace/src/network/tcp_peer.cpp" "CMakeFiles/dfs_network.dir/src/network/tcp_peer.cpp.o" "gcc" "CMakeFiles/dfs_network.dir/src/network/tcp_peer.cpp.o.d"
+  "/home/runner/workspace/src/network/tcp_server.cpp" "CMakeFiles/dfs_network.dir/src/network/tcp_server.cpp.o" "gcc" "CMakeFiles/dfs_network.dir/src/network/tcp_server.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
