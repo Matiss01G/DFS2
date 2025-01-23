3c7ee88429671a1c25df7e5e09d6dc718e76abe1
MarinGallien <22155461-MarinGallien@users.noreply.replit.com>
2025-01-23 18:25:17 +0000

    Checkpoint after starting plan

--- build/tcp_peer_tests[1]_tests.cmake
+++ build/tcp_peer_tests[1]_tests.cmake
@@ -1,5 +1,1 @@
-add_test([=[TCP_PeerTest.BasicConnection]=]  /home/runner/workspace/build/tcp_peer_tests [==[--gtest_filter=TCP_PeerTest.BasicConnection]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[TCP_PeerTest.BasicConnection]=]  PROPERTIES WORKING_DIRECTORY /home/runner/workspace/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test([=[TCP_PeerTest.SendData]=]  /home/runner/workspace/build/tcp_peer_tests [==[--gtest_filter=TCP_PeerTest.SendData]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[TCP_PeerTest.SendData]=]  PROPERTIES WORKING_DIRECTORY /home/runner/workspace/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  tcp_peer_tests_TESTS TCP_PeerTest.BasicConnection TCP_PeerTest.SendData)
+set(  tcp_peer_tests_TESTS)
