1637bfcf90bd8ab5ca36d80ba358953fa348534b
MarinGallien <22155461-MarinGallien@users.noreply.replit.com>
2025-01-27 18:06:44 +0000

    Restored to 'f05bba73cf04bc4102428d1e73e6c75ed10bf1f2'
    Replit-Restored-To:f05bba73cf04bc4102428d1e73e6c75ed10bf1f2

--- src/tests/bootstrap_test.cpp
+++ src/tests/bootstrap_test.cpp
@@ -1,87 +1,15 @@
 #include <gtest/gtest.h>
 #include <thread>
 #include <chrono>
-#include <map>
-#include <sstream>
-#include <mutex>
 #include "network/bootstrap.hpp"
-#include "network/peer_manager.hpp"
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_ostream_backend.hpp>
-#include <boost/log/attributes/current_thread_id.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/expressions/attr.hpp>
+#include "network/peer_manager.hpp"  // Add explicit include for PeerManager
 
 using namespace dfs::network;
-
-// Custom sink that buffers logs by thread ID
-class thread_grouped_sink : public boost::log::sinks::basic_formatted_sink_backend<
-    char,
-    boost::log::sinks::concurrent_feeding
-> {
-    mutable std::mutex mutex_;
-    std::map<std::string, std::stringstream> thread_buffers_;
-
-public:
-    void consume(boost::log::record_view const& rec, string_type const& formatted_message) {
-        std::lock_guard<std::mutex> lock(mutex_);
-
-        // Get thread ID using the correct attribute name
-        auto thread_id_attr = rec.attribute_values()["ThreadID"].extract<boost::log::attributes::current_thread_id::value_type>();
-        if (thread_id_attr) {
-            std::string thread_id = boost::lexical_cast<std::string>(thread_id_attr.get());
-            thread_buffers_[thread_id] << formatted_message << std::endl;
-        }
-    }
-
-    void flush() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        for (const auto& [thread_id, buffer] : thread_buffers_) {
-            std::cout << "\n=== Logs for Thread " << thread_id << " ===" << std::endl;
-            std::cout << buffer.str();
-        }
-        thread_buffers_.clear();
-    }
-};
-
-using sink_t = boost::log::sinks::synchronous_sink<thread_grouped_sink>;
-boost::shared_ptr<sink_t> g_sink;
 
 class BootstrapTest : public ::testing::Test {
 protected:
     const std::string ADDRESS = "127.0.0.1";
     const std::vector<uint8_t> TEST_KEY = std::vector<uint8_t>(32, 0x42); // 32-byte test key
-
-    void SetUp() override {
-        if (!g_sink) {
-            auto backend = boost::make_shared<thread_grouped_sink>();
-            g_sink = boost::make_shared<sink_t>(backend);
-
-            g_sink->set_formatter(
-                boost::log::expressions::stream
-                    << "["
-                    << boost::log::expressions::attr<boost::log::trivial::severity_level>("Severity")
-                    << "] "
-                    << boost::log::expressions::smessage
-            );
-
-            boost::log::core::get()->add_sink(g_sink);
-            boost::log::core::get()->add_global_attribute(
-                "ThreadID",
-                boost::log::attributes::current_thread_id()
-            );
-        }
-    }
-
-    void TearDown() override {
-        if (g_sink) {
-            g_sink->flush();
-        }
-    }
 };
 
 TEST_F(BootstrapTest, PeerConnection) {
@@ -106,12 +34,14 @@ TEST_F(BootstrapTest, PeerConnection) {
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    BOOST_LOG_TRIVIAL(info) << "Getting peer managers";
+    std::cout << "Getting peer managers";
 
     auto& peer1_manager = peer1.get_peer_manager();
     auto& peer2_manager = peer2.get_peer_manager();
 
-    BOOST_LOG_TRIVIAL(info) << "Checking for peer existence in peers map";
+    std::cout << "Joining threads peer managers";
+
+    std::cout << "checking for peer existence in peers map";
     ASSERT_TRUE(peer1_manager.has_peer(PEER2_ID));
     ASSERT_TRUE(peer2_manager.has_peer(PEER1_ID));
     EXPECT_TRUE(peer1_manager.is_connected(PEER2_ID));
@@ -119,4 +49,5 @@ TEST_F(BootstrapTest, PeerConnection) {
 
     peer1_thread.join();
     peer2_thread.join();
+
 }
