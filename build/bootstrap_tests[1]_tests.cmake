f05bba73cf04bc4102428d1e73e6c75ed10bf1f2
MarinGallien <22155461-MarinGallien@users.noreply.replit.com>
2025-01-27 17:59:47 +0000

    Checkpoint after starting plan

--- build/bootstrap_tests[1]_tests.cmake
+++ build/bootstrap_tests[1]_tests.cmake
@@ -1,5 +1,3 @@
 add_test([=[BootstrapTest.PeerConnection]=]  /home/runner/workspace/build/bootstrap_tests [==[--gtest_filter=BootstrapTest.PeerConnection]==] --gtest_also_run_disabled_tests)
 set_tests_properties([=[BootstrapTest.PeerConnection]=]  PROPERTIES WORKING_DIRECTORY /home/runner/workspace/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test([=[BootstrapTest.ShutdownTest]=]  /home/runner/workspace/build/bootstrap_tests [==[--gtest_filter=BootstrapTest.ShutdownTest]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[BootstrapTest.ShutdownTest]=]  PROPERTIES WORKING_DIRECTORY /home/runner/workspace/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  bootstrap_tests_TESTS BootstrapTest.PeerConnection BootstrapTest.ShutdownTest)
+set(  bootstrap_tests_TESTS BootstrapTest.PeerConnection)
