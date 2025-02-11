2ba655b2e773ab095df97b0b33ca764b009dd36c
Marin Gallien <139711467+Matiss01G@users.noreply.github.com>
2025-02-11 14:49:09 +0000

    11/02/2025 -1

--- src/crypto/crypto_stream.cpp
+++ src/crypto/crypto_stream.cpp
@@ -36,7 +36,7 @@ struct CipherContext {
 };
 
 //==============================================
-// CONSTRUCTOR AND DESTRUYC
+// CONSTRUCTOR AND DESTRUCTOR
 //==============================================
 
 CryptoStream::CryptoStream() {
